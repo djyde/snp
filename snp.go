97d52f5e2a1004433cdd436652e4b7d6c1e9053c
Randy <randypriv@gmail.com>
2020-12-24 20:12:33 +0800

    indent

--- snp.go
+++ snp.go
@@ -26,6 +26,7 @@ func JSONMarshal(t interface{}) ([]byte, error) {
 	buffer := &bytes.Buffer{}
 	encoder := json.NewEncoder(buffer)
 	encoder.SetEscapeHTML(false)
+	encoder.SetIndent("", "  ")
 	err := encoder.Encode(t)
 	return buffer.Bytes(), err
 }
